ca1ef7844d9ea22062a58cb879cd65bb1c33bc61
mattanikiej <mattanikiej@gmail.com>
2022-11-20 12:07:06 -0500

    initial commit

---
+++ soundboard.js
@@ -0,0 +1,38 @@
+// boing listeners
+var boingButton = document.getElementById("boing");
+
+boingButton.addEventListener('click', ()=>{
+    var audioFile = document.getElementById('boing-audio');
+    audioFile.play();
+});
+
+boingButton.addEventListener('mouseenter', ()=>{
+    var audioFile = document.getElementById('boing-audio');
+    audioFile.play();
+});
+
+// boxing listeners
+var boxingButton = document.getElementById("boxing");
+
+boxingButton.addEventListener('click', ()=>{
+    var audioFile = document.getElementById('boxing-audio');
+    audioFile.play();
+});
+
+boxingButton.addEventListener('mouseenter', ()=>{
+    var audioFile = document.getElementById('boxing-audio');
+    audioFile.play();
+});
+
+// coin listeners
+var coinButton = document.getElementById("coin");
+
+coinButton.addEventListener('click', ()=>{
+    var audioFile = document.getElementById('coin-audio');
+    audioFile.play();
+});
+
+coinButton.addEventListener('mouseenter', ()=>{
+    var audioFile = document.getElementById('coin-audio');
+    audioFile.play();
+});
